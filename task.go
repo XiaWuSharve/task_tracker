dd7dae39039d160eb346b6be1b2847db6462e7b4
XiaWuSharve <sharve@foxmail.com>
2025-11-06 19:14:11 +0800

    UPDATE: most of the interface except file system

--- task.go
+++ task.go
@@ -1,6 +1,8 @@
 package tasktracker
 
 import (
+	"errors"
+	"fmt"
 	"time"
 )
 
@@ -15,15 +17,74 @@ type Task struct {
 type TaskRepository []Task
 
 type TaskRepositoryAction interface {
-	Add(task Task)
-	Update(source Task, target Task)
-	Delete(task Task)
-	MarkAs(task Task, status Status)
-	ListAll() []Task
-	ListBy(status Status) []Task
+	Add(task Task) (*TaskRepository, error)
+	Update(sourceId int, target Task) (*TaskRepository, error)
+	Delete(taskId int) (*TaskRepository, error)
+	MarkAs(taskId int, status Status) (*TaskRepository, error)
+	ListAll() TaskRepository
+	listBy(status Status) TaskRepository
+	ListDone() TaskRepository
+	ListNotDone() TaskRepository
+	ListInProgress() TaskRepository
+	findOneById(id int) int
 }
 
-func (t *TaskRepository) Add(task Task) *TaskRepository {
+func (t *TaskRepository) findOneById(id int) int {
+	for i := range *t {
+		if (*t)[i].Id == id {
+			return i
+		}
+	}
+	return -1
+}
+
+func (t *TaskRepository) Add(task Task) (*TaskRepository, error) {
+	existingTaskIndex := t.findOneById(task.Id)
+	if existingTaskIndex != -1 {
+		errMsg := fmt.Sprintf("Except no existing task, found task %d on %d", task.Id, existingTaskIndex)
+		return t, errors.New(errMsg)
+	}
 	*t = append(*t, task)
-	return t
+	return t, nil
+}
+
+func (t *TaskRepository) Update(sourceId int, target Task) (*TaskRepository, error) {
+	existingTaskIndex := t.findOneById(sourceId)
+	if existingTaskIndex == -1 {
+		errMsg := fmt.Sprintf("Except existing task %d, found no existing task", sourceId)
+		return t, errors.New(errMsg)
+	}
+
+	(*t)[existingTaskIndex] = target
+	return t, nil
+}
+
+func (t *TaskRepository) Delete(taskId int) (*TaskRepository, error) {
+	existingTaskIndex := t.findOneById(taskId)
+	if existingTaskIndex == -1 {
+		errMsg := fmt.Sprintf("Except existing task %d, found no existing task", taskId)
+		return t, errors.New(errMsg)
+	}
+
+	*t = append((*t)[:existingTaskIndex], (*t)[existingTaskIndex+1:]...)
+
+	return t, nil
+}
+
+func (t *TaskRepository) MarkAs(taskId int, status Status) (*TaskRepository, error) {
+	existingTaskIndex := t.findOneById(taskId)
+	if existingTaskIndex == -1 {
+		errMsg := fmt.Sprintf("Except existing task %d, found no existing task", taskId)
+		return t, errors.New(errMsg)
+	}
+
+	(*t)[existingTaskIndex].Status = status
+
+	return t, nil
+}
+
+func (t *TaskRepository) ListAll() TaskRepository {
+	dest := make(TaskRepository, len(*t))
+	copy(dest, *t)
+	return dest
 }
--- task_test.go
+++ task_test.go
@@ -1,6 +1,7 @@
 package tasktracker
 
 import (
+	"fmt"
 	"testing"
 	"time"
 )
@@ -17,5 +18,40 @@ func TestAddTask(t *testing.T) {
 	expected := 1
 	if len(tasks) != expected {
 		t.Errorf("expected: %d, got len of tasks: %d", expected, len(tasks))
+	}
+}
+
+func TestUpdateTask(t *testing.T) {
+	var tasks TaskRepository
+	tasks.Add(Task{
+		Id:          1,
+		Description: "hello world",
+		Status:      inProgress,
+		CreatedAt:   time.Now(),
+		UpdatedAt:   time.Now(),
+	})
+
+	expected := Task{
+		Id:          1,
+		Description: "hello Sharve",
+		Status:      done,
+		CreatedAt:   time.Date(2222, 12, 12, 12, 12, 12, 12, time.Now().Location()),
+		UpdatedAt:   time.Date(2222, 12, 12, 12, 12, 12, 12, time.Now().Location()),
+	}
+
+	tasks.Update(1, Task{
+		Id:          1,
+		Description: "hello Sharve",
+		Status:      done,
+		CreatedAt:   time.Date(2222, 12, 12, 12, 12, 12, 12, time.Now().Location()),
+		UpdatedAt:   time.Date(2222, 12, 12, 12, 12, 12, 12, time.Now().Location()),
+	})
+
+	if len(tasks) != 1 {
+		t.Errorf("expected: %d, got len of tasks: %d", 1, len(tasks))
+	}
+
+	if tasks[0] != expected {
+		t.Errorf("expected: %s, got %s", fmt.Sprint(expected), fmt.Sprint(tasks[0]))
 	}
 }
